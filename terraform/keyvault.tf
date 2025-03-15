16900c3d9c26d6ea0f271e19b77b7a10558643bb
Veronique Tavares <125508905+2166618@users.noreply.github.com>
2025-03-15 12:00:37 -0400

    Ajout à fichier kv

--- terraform/keyvault.tf
+++ terraform/keyvault.tf
@@ -1,4 +1,5 @@
-# keyvault.tf
+# Obtenir les informations du client Azure courant
+data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "kv" {
   name                        = "kv-dev-calicot-cc-${var.unique_code}"
@@ -7,6 +8,30 @@ resource "azurerm_key_vault" "kv" {
   enabled_for_deployment      = true
   enabled_for_disk_encryption = true
   enabled_for_template_deployment = true
+  
+  # Éléments manquants ajoutés
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  sku_name                    = "standard"
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+  
+  # Politique d'accès pour le service principal qui déploie les ressources
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+    
+    key_permissions = [
+      "Get", "List", "Create", "Delete", "Update"
+    ]
+    
+    secret_permissions = [
+      "Get", "List", "Set", "Delete"
+    ]
+    
+    certificate_permissions = [
+      "Get", "List", "Create", "Delete"
+    ]
+  }
 }
 
 resource "azurerm_key_vault_secret" "secret" {
